cb67a52a9b56cd28a59621436ac45fe2167c60a0
John Wu <webmaster@leapoahead.com>
2013-11-25 22:59:52 +0800

    add user listing/registering tests

---
+++ helper/common.js
@@ -0,0 +1,14 @@
+commonHelper =  {}
+
+commonHelper.randomString = function(len) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i = 0 ; i < len ; i ++ ) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return text;
+}
+
+module.exports = commonHelper;
--- test/users.js
+++ test/users.js
@@ -1,15 +1,16 @@
 var should = require('should'),
-request = require('request');
+request = require('request'),
+_ = require('underscore'),
+commonHelper = require('../helper/common.js');
 
 describe('Users Controller Tester', function(){
-  var host = 'http://l:3000';
+  var host = 'http://l:3000',
+  token_id = null;
   var auth_header = {
     'AUTH_EMAIL': 'webmaster@leapoahead.com',
     'AUTH_TOKEN': 'theusertoken'
   };
-  var token_id;
-
-  beforeEach(function(done){
+  var login = function(done){
     var config = {};
     config.method = 'POST';
     config.url = host + '/sessions';
@@ -22,19 +23,25 @@ describe('Users Controller Tester', function(){
       token_id = (JSON.parse(body))['id'];
       done();
     });
-  });
-
-  afterEach(function(){
+  };
+  var logout = function(done){
     var config = {};
     config.method = 'DELETE';
     config.url = host + '/sessions/' + token_id;
     config.headers = auth_header;
     request(config, function(err, res, body) {
       if (err) return console.log(err);
+      token_id = null;
+      done();
     });
-  });
+  };
+
+
 
   describe('User follow tester', function(){
+    beforeEach(login);
+    afterEach(logout);
+
     it('Should be able to follow a user', function(done){
       var config = {};
       config.method = 'POST';
@@ -54,6 +61,7 @@ describe('Users Controller Tester', function(){
       config.url = host + '/users/2/followship';
       config.headers = auth_header;
       request(config, function(err, res, body) {
+        if (err) return console.log(err);
         result = JSON.parse(body);
         result.should.have.property('status');
         result.status.should.be.exactly(1);
@@ -80,6 +88,7 @@ describe('Users Controller Tester', function(){
       config.url = host + '/users/2/followship';
       config.headers = auth_header;
       request(config, function(err, res, body) {
+        if (err) return console.log(err);
         result = JSON.parse(body);
         result.should.have.property('status');
         result.status.should.be.exactly(0);
@@ -88,5 +97,46 @@ describe('Users Controller Tester', function(){
     });
   }); // User follow tester
 
+
+  describe('User registration', function(){ 
+    it('should be able to register', function(done){
+      var config = {};
+      config.method = 'POST';
+      config.url = host +  '/users/';
+      config.form = {
+        'email': commonHelper.randomString(9) + '@leapoahead.com',
+        'encrypted_password': commonHelper.randomString(10),
+        'nickname': commonHelper.randomString(7)
+      }
+      request(config, function(err, res, body){
+        if (err) return console.log(err);
+        result = JSON.parse(body);
+        result.should.have.property('id');
+        result.should.have.property('auth_token');
+        done();
+      });
+    })
+  }); // User registration tester
+
+  describe('User listing(auto complete) tester', function(){
+    it('should be able to auto complete', function(done) {
+      var config = {};
+      config.method = 'GET';
+      config.url = host + '/users';
+      config.qs = {
+        'auto_complete_word' : 'w',
+        'count' : 4
+      };
+      request(config, function(err, res, body) {
+        if (err) return console.log(err);
+        result = JSON.parse(body);
+        _.each(result, function(item){
+          item.should.have.property('nickname', 'avatar', 'school', 'speciality', 'gender', 'id');
+          item['nickname'].should.match(/^.*w.*$/);
+        });
+        done();
+      });
+    });
+  }); // User listing tester
 
 });
