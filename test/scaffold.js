bd7546f1798aed649d43105800c6b1e6ee40d5fa
John Wu <webmaster@leapoahead.com>
2013-11-26 02:28:36 +0800

    add scaffold for future use

---
+++ test/scaffold.js
@@ -0,0 +1,40 @@
+var should = require('should');
+var request = require('request');
+var _ = require('underscore');
+var config = require('../config.js');
+var commonHelper = require('../helper/common.js');
+
+
+var host = config.host;
+var token_id = null;
+var auth_header = {
+  'AUTH_EMAIL': 'webmaster@leapoahead.com',
+  'AUTH_TOKEN': 'theusertoken'
+};
+var login = function(done){
+  var config = {};
+  config.method = 'POST';
+  config.url = host + '/sessions';
+  config.form = {
+    'email': 'webmaster@leapoahead.com',
+    'encrypted_password': 'abcabc'
+  };
+  request(config, function(err, res, body) {
+    if (err) return console.log(err);
+    var result=JSON.parse(body);
+    token_id = result['id'];
+    auth_header['AUTH_TOKEN'] = result['auth_token'];
+    done();
+  });
+};
+var logout = function(done){
+  var config = {};
+  config.method = 'DELETE';
+  config.url = host + '/sessions/' + token_id;
+  config.headers = auth_header;
+  request(config, function(err, res, body) {
+    if (err) return console.log(err);
+    token_id = null;
+    done();
+  });
+};
